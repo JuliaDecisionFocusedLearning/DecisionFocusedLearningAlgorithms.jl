07965cccff70f1082652047b369a5e57d59235cc
Documenter.jl <documenter@juliadocs.github.io>
2025-08-27 08:37:39 +0000

    build based on 42ed250

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#DecisionFocusedLearningAlgorithms","page":"Home","title":"DecisionFocusedLearningAlgorithms","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for DecisionFocusedLearningAlgorithms.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
